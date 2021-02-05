47b508b08be79c4ef80ec6a71eef74eb13527752
koudenpa <koudenpa@hotmail.com>
2021-02-06 01:30:23 +0900

    こんな感じかってクエリ飛ばせた。

--- redash/query_runner/mackerel.py
+++ redash/query_runner/mackerel.py
@@ -1,74 +1,104 @@
-# -*- encoding: utf-8 -*-
-import csv as csv
-import json
 import requests
+import time
+import logging
+from datetime import datetime
+from dateutil import parser
+from redash.query_runner import BaseQueryRunner, register, TYPE_DATETIME, TYPE_STRING, TYPE_FLOAT
+from redash.utils import json_dumps
 
-from redash.query_runner import BaseQueryRunner, register
 
-# https://discuss.redash.io/t/creating-a-new-query-runner-data-source-in-redash/347
 class Mackerel(BaseQueryRunner):
+    should_annotate_query = False
+
     @classmethod
     def configuration_schema(cls):
         return {
             'type': 'object',
             'properties': {
-                'apikey': {
+                'api_key': {
                     'type': 'string',
                     'title': 'API-Key'
                 },
+                'base_url': {
+                    'type': 'string',
+                    "default": 'https://api.mackerelio.com',
+                    'title': 'Base URL'
+                },
             },
-            "order": ['apikey'],
-            'required': ['apikey'],
-            'secret': ['apikey'],
+            "order": ['api_key', 'base_url'],
+            'required': ['api_key'],
+            'secret': ['api_key'],
         }
-
-    @classmethod
-    def annotate_query(cls):
-        return False
-
-    def __init__(self, configuration):
-        super(Mackerel, self).__init__(configuration)
 
     def test_connection(self):
-        # TODO なんか取る
-        pass
+        api_key = str(self.configuration.get('api_key'))
+        headers = {'X-Api-Key': api_key}
+        resp = requests.get(self.configuration.get("base_url", None), headers=headers)
+        return resp.ok
+
+    def get_schema(self, get_stats=False):
+        base_url = self.configuration["base_url"]
+        api_key = str(self.configuration.get('api_key'))
+        headers = {'X-Api-Key': api_key}
+        metrics_path = "/api/v0/services"
+        response = requests.get(base_url + metrics_path, headers=headers)
+        response.raise_for_status()
+        services = response.json()["services"]
+
+        schema = {}
+        for name in services:
+            schema[name] = {"name": name, "columns": []}
+        return list(schema.values())
 
     def run_query(self, query, user):
-        # https://ameblo.jp/gcrest-engineer/entry-12339623721.html
-        apikey = str(self.configuration.get('apikey'))
-        metrics = query.strip().splitlines()
+        """
+        Ref. https://mackerel.io/ja/api-docs/
 
-        data = {
-            'columns': metrics,
-            'rows': [],
-        }
+        example: host/service metrics
+            from=2021-02-01T00:00:00Z
+            to=2021-02-01T01:00:00Z
+            /hosts/<hostId>/metrics?name=<metricName>
+            /services/<serviceName>/metrics?name=<metricName>
+        """
 
-        for row in csv.DictReader(query.strip().splitlines(), delimiter=delimiter):
-            if len(data['columns']) == 0:
-                for key in row.keys():
-                    data['columns'].append({'name': key, 'friendly_name': key})
+        base_url = self.configuration["base_url"]
+        api_key = str(self.configuration.get('api_key'))
+        headers = {'X-Api-Key': api_key}
+        columns = [
+            {"friendly_name": "timestamp", "type": TYPE_DATETIME, "name": "timestamp"},
+        ]
+        results = {}
 
-            data['rows'].append(row)
+        try:
+            error = None
+            query = query.strip()
+            queries = query.strip().splitlines()
 
-        return json.dumps(data), None
+            api_endpoint = base_url + query
 
-        response = requests.get('https://api.mackerelio.com/api/v0/services/<serviceName>/metrics', headers={'X-Api-Key': apikey})
-        response_json = response.json()
-        data = {
-            'columns': [
-                {'name': 'id', 'friendly_name': 'id'},
-                {'name': 'status', 'friendly_name': 'status'},
-                {'name': 'monitorId', 'friendly_name': 'monitorId'},
-                {'name': 'hostId', 'friendly_name': 'hostId'},
-                {'name': 'value', 'friendly_name': 'value'},
-                {'name': 'message', 'friendly_name': 'message'},
-                {'name': 'reason', 'friendly_name': 'reason'},
-                {'name': 'openedAt', 'friendly_name': 'openedAt'},
-                {'name': 'closedAt', 'friendly_name': 'closedAt'},
-            ],
-            'rows': response_json['alerts'],
-        }
+            response = requests.get(api_endpoint, headers=headers)
+            response.raise_for_status()
 
-        return json.dumps(data), None
+            metrics = response.json()["metrics"]
+            logging.info(json_dumps(metrics))
+
+            columns.append(
+                {
+                    "friendly_name": query,
+                    "type": TYPE_FLOAT,
+                    "name": query,
+                }
+            )
+
+            rows = [dict(zip(["timestamp", query], [datetime.fromtimestamp(metric['time']), metric['value']])) for metric in metrics]
+
+            logging.info(json_dumps({"rows": rows, "columns": columns}))
+            json_data = json_dumps({"rows": rows, "columns": columns})
+
+        except requests.RequestException as e:
+            return None, str(e)
+
+        return json_data, error
+
 
 register(Mackerel)
